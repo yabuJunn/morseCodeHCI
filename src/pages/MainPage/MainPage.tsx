ad8144d34136431a6ddc6692abedd6ca9633f02d
yabuJunn <juancho04reyes@gmail.com>
2024-10-08 22:14:55 -0500

    Added navigation

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,19 +1,23 @@
+import { useNavigate } from 'react-router-dom'
+
 import './MainPage.css'
 
 export const MainPage = () => {
+    const navigation = useNavigate()
+
     return <>
         <main className='page'>
             <div id='superiorMenu'>
-                <button id='learnButton'>
+                <button id='learnButton' onClick={() => { navigation('/learn') }}>
                     Aprender
                 </button>
 
-                <button id='practiceButton'>
+                <button id='practiceButton' onClick={() => { navigation('/practice') }}>
                     Practicar
                 </button>
             </div>
 
-            <button id='abcButton'>
+            <button id='abcButton' onClick={() => { navigation('/alphabet') }}>
                 Abecedario
             </button>
         </main>
